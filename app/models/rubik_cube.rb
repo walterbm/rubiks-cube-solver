87858e3b4f3dc42d43c0b43719286d82a9b29328
walterbm <walter.beller.morales@gmail.com>
2015-08-27 02:27:16 -0400

    refactor cube turning in model to apply permutation

--- app/models/rubik_cube.rb
+++ app/models/rubik_cube.rb
@@ -1,17 +1,17 @@
 class RubikCube
 
-  DEFAULT_CUBE = ["front_left_up", "left_up_front", "up_front_left", 
-                  "front_up_right", "up_right_front", "right_front_up", 
-                  "front_down_left", "down_left_front", "left_front_down", 
-                  "front_right_down", "right_down_front", "down_front_right", 
-                  "back_up_left", "up_left_back", "left_back_up", 
-                  "back_right_up", "right_up_back", "up_back_right", 
-                  "back_left_down", "left_down_back", "down_back_left", 
-                  "back_down_right", "down_right_back", "right_back_down"]
+   # ["front_left_up", "left_up_front", "up_front_left", 
+   #  "front_up_right", "up_right_front", "right_front_up", 
+   #  "front_down_left", "down_left_front", "left_front_down", 
+   #  "front_right_down", "right_down_front", "down_front_right", 
+   #  "back_up_left", "up_left_back", "left_back_up", 
+   #  "back_right_up", "right_up_back", "up_back_right", 
+   #  "back_left_down", "left_down_back", "down_back_left", 
+   #  "back_down_right", "down_right_back", "right_back_down"]
 
   attr_accessor :cube
 
-  def initialize(cube_configuration = DEFAULT_CUBE)
+  def initialize(cube_configuration)
     @cube = cube_configuration
   end
 
@@ -24,72 +24,43 @@ class RubikCube
   end
 
   def front_clockwise
-    cube[0...3], cube[3...6], cube[6...9], cube[9...12] = cube[6...9], cube[0...3], cube[9...12], cube[3...6]
-    cube
+    self.cube = apply_permutation([6,7,8,0,1,2,9,10,11,3,4,5,12,13,14,15,16,17,18,19,20,21,22,23])
   end
 
   def front_counterclockwise
-    cube[0...3], cube[3...6], cube[6...9], cube[9...12] = cube[3...6], cube[9...12], cube[0...3], cube[6...9]
-    cube
+    self.cube = apply_permutation([3,4,5,9,10,11,0,1,2,6,7,8,12,13,14,15,16,17,18,19,20,21,22,23])
   end
 
   def left_clockwise
     # [0,1,2,     3,4,5,    6,7,8,    9,10,11,    12,13,14,   15,16,17,   18,19,20,   21,22,23]
     # [13,14,12,  3,4,5,    2,0,1,    9,10,11,    20,18,19,   15,16,17,   7,8,6,      21,22,23]
 
-    cube[0], cube[1], cube[2], 
-    cube[6], cube[7], cube[8], 
-    cube[12], cube[13], cube[14],
-    cube[18], cube[19], cube[20] =  cube[13], cube[14], cube[12], 
-                                    cube[2], cube[0], cube[1],
-                                    cube[20], cube[18], cube[19],
-                                    cube[7], cube[8], cube[6]
-    cube
+    self.cube = apply_permutation([13,14,12,3,4,5,2,0,1,9,10,11,20,18,19,15,16,17,7,8,6,21,22,23])
   end
 
   def left_counterclockwise
     # [0,1,2,    3,4,5,   6,7,8,     9,10,11,   12,13,14,   15,16,17,    18,19,20,     21,22,23]
     # [7,8,6,    3,4,5,   20,18,19,  9,10,11,   2,0,1,      15,16,17,    13,14,12,     21,22,23]
 
-    cube[0], cube[1], cube[2], 
-    cube[6], cube[7], cube[8], 
-    cube[12], cube[13], cube[14],
-    cube[18], cube[19], cube[20] =  cube[7], cube[8], cube[6],
-                                    cube[20], cube[18], cube[19], 
-                                    cube[2], cube[0], cube[1],
-                                    cube[13], cube[14], cube[12]
-                                    
-    cube
+    self.cube = apply_permutation([7,8,6,3,4,5,20,18,19,9,10,11,2,0,1,15,16,17,13,14,12,21,22,23])
   end
 
   def up_clockwise
     # [0,1,2,   3,4,5,      6,7,8,   9,10,11,  12,13,14,  15,16,17,   18,19,20,  21,22,23]
     # [5,3,4,   16,17,15,   6,7,8,   9,10,11,  1,2,0,     14,12,13,   18,19,20,  21,22,23]
 
-    cube[0], cube[1], cube[2], 
-    cube[3], cube[4], cube[5], 
-    cube[12], cube[13], cube[14],
-    cube[15], cube[16], cube[17] =  cube[5], cube[3], cube[4],
-                                    cube[16], cube[17], cube[15], 
-                                    cube[1], cube[2], cube[0],
-                                    cube[14], cube[12], cube[13]
-                                    
-    cube
+    self.cube = apply_permutation([5,3,4,16,17,15,6,7,8,9,10,11,1,2,0,14,12,13,18,19,20,21,22,23])
   end
 
   def up_counterclockwise
     # [0,1,2,     3,4,5,      6,7,8,   9,10,11,  12,13,14,  15,16,17,   18,19,20,  21,22,23]
     # [14,12,13   1,2,0       6,7,8    9,10,11,  16,17,15   5,3,4       18,19,20,  21,22,23]
 
-    cube[0], cube[1], cube[2], 
-    cube[3], cube[4], cube[5], 
-    cube[12], cube[13], cube[14],
-    cube[15], cube[16], cube[17] =  cube[14], cube[12], cube[13],
-                                    cube[1], cube[2], cube[0], 
-                                    cube[16], cube[17], cube[15],
-                                    cube[5], cube[3], cube[4]
-                                    
-    cube
+    self.cube = apply_permutation([14,12,13,1,2,0,6,7,8,9,10,11,16,17,15,5,3,4,18,19,20,21,22,23])
+  end
+
+  def apply_permutation(permutation)
+    permutation.collect { |index| @cube[index] }
   end
 
 end
--- spec/models/rubik_cube_spec.rb
+++ spec/models/rubik_cube_spec.rb
@@ -6,12 +6,9 @@ RSpec.describe RubikCube, :type => :model do
     @test_cube = RubikCube.new(@test_cube_array)
   end
 
-  it "returns the current cube array" do
-    expect(@test_cube.print).to eq(@test_cube_array)
-  end
-
   it "rotates the front face clockwise" do 
-    expect(@test_cube.quarter_turn("front_clockwise")).to eq([6,7,8,0,1,2,9,10,11,3,4,5,12,13,14,15,16,17,18,19,20,21,22,23])
+    @test_cube.quarter_turn("front_clockwise")
+    expect(@test_cube.cube).to eq([6,7,8,0,1,2,9,10,11,3,4,5,12,13,14,15,16,17,18,19,20,21,22,23])
   end
 
   it "rotates the front face counter-clockwise" do 

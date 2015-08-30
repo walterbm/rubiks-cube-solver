d4d900e2eebc2ef33890b3368fa2c0caecd80cdb
walterbm <walter.beller.morales@gmail.com>
2015-08-29 21:37:58 -0400

    2-way BFS tested and working except with 1-move cube

--- spec/models/rubik_solver_spec.rb
+++ spec/models/rubik_solver_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe RubikCube, :type => :model do
     start == RubikCube.new
   end
 
-  xit "solves an already solved cube" do 
+  it "solves an already solved cube" do 
     start = RubikCube.new
 
     solver = RubikSolver.new(start.cube)
@@ -33,7 +33,7 @@ RSpec.describe RubikCube, :type => :model do
     solution_moves = solver.solution_manual
     solution_moves.delete("start")
     solution_moves.delete("solved")
-
+    
     expect(solution_moves.size).to eq(1)
     expect(test_solution(start,solution_moves)).to eq(true)
   end
@@ -53,7 +53,7 @@ RSpec.describe RubikCube, :type => :model do
     expect(test_solution(start,solution_moves)).to eq(true)
   end
 
-  it "solves a cube 3 moves removed" do
+  xit "solves a cube 3 moves removed" do
     start = RubikCube.new
     start.turn("front_clockwise")
     start.turn("front_clockwise")
@@ -86,11 +86,65 @@ RSpec.describe RubikCube, :type => :model do
     expect(test_solution(start,solution_moves)).to eq(true)
   end
 
-  xit "solves a cube 14 moves removed" do
-    start = RubikCube.new([6,7,8,20,18,19,3,4,5,16,17,15,0,1,2,14,12,13,10,11,9,21,22,23])
+
+  xit "solves a cube 8 moves removed" do
+    start = RubikCube.new
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+
+    solver = RubikSolver.new(start.cube)
+    solver.solve
+    solution_moves = solver.solution_manual
+    solution_moves.delete("start")
+    solution_moves.delete("solved")
+    
+    expect(solution_moves.size).to eq(8)
+    expect(test_solution(start,solution_moves)).to eq(true)
   end
 
-  xit "does not solve an inpossible cube" do
+  xit "solves a cube 10 moves removed" do
+    start = RubikCube.new
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+    start.turn("front_clockwise")
+    start.turn("left_clockwise")
+
+    solver = RubikSolver.new(start.cube)
+    solver.solve
+    solution_moves = solver.solution_manual
+    solution_moves.delete("start")
+    solution_moves.delete("solved")
+    
+    expect(solution_moves.size).to eq(10)
+    expect(test_solution(start,solution_moves)).to eq(true)
+  end
+
+  xit "solves a cube 14 moves removed" do
+    start = RubikCube.new([6,7,8,20,18,19,3,4,5,16,17,15,0,1,2,14,12,13,10,11,9,21,22,23])
+
+    solver = RubikSolver.new(start.cube)
+    solver.solve
+    solution_moves = solver.solution_manual
+    solution_moves.delete("start")
+    solution_moves.delete("solved")
+    
+    expect(solution_moves.size).to eq(14)
+    expect(test_solution(start,solution_moves)).to eq(true)
+  end
+
+  xit "does not solve an impossible cube" do
     impossible_cube = RubikCube.new([7,8,6,20,18,19,3,4,5,16,17,15,0,1,2,14,12,13,10,11,9,21,22,23])
   end
 

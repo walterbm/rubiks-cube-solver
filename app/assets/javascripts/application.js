7ab1b88f1ae6efeb1f876d362ab1d1393804dbc0
walterbm <walter.beller.morales@gmail.com>
2015-09-05 23:55:20 -0400

    add vanilla boostrap and basic styling for page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 
-//= require three_old.js
-//= require Tween.js
-//= require colorpicker.min.js
+//= require three.min.js
+//= require tween.min.js
 
 //= require rubik.js
 //= require rubikApplication.js
